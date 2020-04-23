14bfd008b6279fa01ee52337d3e49262118adecb
Christopher Ksiazak <cksiazak@gmail.com>
2020-04-23 09:35:20 -0600

    initialize repo, testing code block

---
+++ src/components/codeEditor.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import AceEditor from 'react-ace'
+
+import 'ace-builds/src-noconflict/mode-json'
+import 'ace-builds/src-noconflict/theme-github'
+
+const CodeEditor = (): JSX.Element => {
+  const [code, setCode] = useState<string>('{}')
+
+  const onChange = (e: string)=> {
+    setCode(e)
+ }
+  return (
+    <AceEditor
+      mode='json'
+      theme='github'
+      value={code}
+      onChange={onChange}
+      name='code-editor'
+      editorProps={{ $blockScrolling: true }}
+    />
+  )
+}
+
+export default CodeEditor
